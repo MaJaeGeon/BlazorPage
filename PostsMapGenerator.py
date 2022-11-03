41773e0af038dc95a0e790196dc2c25ea35aa59a
MaJaeGeon <akworjs0517@gmail.com>
2022-11-03 14:31:51 +0000

    test

--- PostsMapGenerator.py
+++ PostsMapGenerator.py
@@ -2,13 +2,17 @@ import os
 import json
 
 def path_to_dict(path):
-    d = {'name': os.path.basename(path), 'path': os.path.relpath(path)}
+    d = {
+        'name': os.path.basename(path), 
+        'path': os.path.relpath(path)
+        }
     if os.path.isdir(path):
         d['type'] = "directory"
         d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
         (path)]
     else:
         d['type'] = "file"
+        d['extention'] = os.path.splitext(path)[1]
     return d
 
 with open("PostsMap.json", 'w') as file:
