8eb602f67bd3f742c51cdca56207e2ce52122343
MaJaeGeon <akworjs0517@gmail.com>
2022-11-05 13:34:02 +0000

    test

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,22 @@
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorPage.Pages {
+    public partial class Index : ComponentBase {
+            [Inject]
+            protected HttpClient Client {get; set;} = null!;
+            protected Root? Root {get;set;}
+
+            protected override async Task OnParametersSetAsync() {
+                    Root = await Client.GetFromJsonAsync<Root>("/PostsMap.json");
+            }
+    }
+
+    public class Root {
+            public string? Name { get; set; }
+            public string? Path { get; set; }
+            public string? Extention { get; set; }
+            public string? Type { get; set; }
+            public List<Root>? Children { get; set; }
+    }
+}
