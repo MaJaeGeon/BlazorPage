189f52cb9a8b8219ead214d3452100ce8faa57e5
MaJaeGeon <59637845+MaJaeGeon@users.noreply.github.com>
2022-11-05 14:12:58 +0900

    Update PostsMapGenerator.py

--- PostsMapGenerator.py
+++ PostsMapGenerator.py
@@ -6,17 +6,15 @@ def path_to_dict(path):
         'name': os.path.basename(path), 
         'path': os.path.relpath(path)
         }
-    extention = os.path.splitext(path)[1]
-    
+
+    d['extention'] = os.path.splitext(path)[1]
+
     if os.path.isdir(path):
         d['type'] = "directory"
-        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
-        (path)]
+        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir(path)]
     else:
-        if extention == ".md" or extention == ".html":
             d['type'] = "file"
-            d['extention'] = extention
     return d
 
 with open("PostsMap.json", 'w') as file:
-    json.dump(path_to_dict('../'), file) 
+    json.dump(path_to_dict('./posts/'), file) 
