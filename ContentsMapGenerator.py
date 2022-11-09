e2e0c292f5ebc98dc81273b7144095ac1782baa7
MaJaeGeon <akworjs0517@gmail.com>
2022-11-09 13:35:18 +0000

    test

--- ContentsMapGenerator.py
+++ ContentsMapGenerator.py
@@ -5,18 +5,15 @@ import sys
 def path_to_dict(path):
     d = {
         'name': os.path.basename(path), 
-        'path': os.path.relpath(path)
+        'path': os.path.relpath(path),
+        'extention': os.path.splitext(path)[1]
         }
-    extention = os.path.splitext(path)[1]
 
     if os.path.isdir(path):
         d['type'] = "directory"
         d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir(path)]
     else:
-        if extention == "": return
-
         d['type'] = "file"
-        d['extention'] = extention
     return d
 
 with open("%sContentsMap.json" % sys.argv[1], 'w') as file:
